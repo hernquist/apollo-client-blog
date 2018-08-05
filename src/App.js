cc9ed07b8783902d6ac459d9825578874c52b074
david hernquist <dhernquist@rentpath.com>
2018-08-04 23:20:41 -0400

    apollo provider installed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import logo from './logo.svg';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.css';
 
 const client = new ApolloClient({
@@ -10,15 +11,16 @@ const client = new ApolloClient({
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </ApolloProvider>
     );
   }
 }
