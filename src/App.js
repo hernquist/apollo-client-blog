40d6da7d63e0fb4a5d2a105409ce17c7f3a77e8c
david hernquist <dhernquist@rentpath.com>
2018-08-07 22:08:31 -0400

    persisting cache

--- src/App.js
+++ src/App.js
@@ -1,51 +1,28 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Post from './Posts/Post';
 import Posts from './Posts/Posts';
 import NewPost from './Posts/NewPost';
 import './App.css';
 
- const defaultState = {
-   isEditMode: false
- } 
-
-const client = new ApolloClient({
-  uri: 'https://api-useast.graphcms.com/v1/cjkg8z3wf0jan01fhumzt7z56/master',
-  clientState: {
-    defaults: defaultState,
-    resolvers: {}
-  }
-})
-
-// Using the client and a query outside of react
-// client.query({
-//   query: POSTS_QUERY
-// }).then(
-//   res => console.log(res) 
-// )
-
 class App extends Component {
   render() {
     return (
       <Router>
-        <ApolloProvider client={client}>
-          <div className="App">
-            <header className="App-header">
-              <h1 className="App-title">React-Apollo-GraphQL-GraphCMS</h1>
-              <Link to='/'>HOME</Link>
-            </header>
-            
-            <main>
-              <Switch>
-                <Route exact path="/" component={Posts} />
-                <Route path="/post/new" component={NewPost} />
-                <Route path="/post/:id" component={Post} />
-              </Switch>
-            </main>
-          </div>
-        </ApolloProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">React-Apollo-GraphQL-GraphCMS</h1>
+            <Link to='/'>HOME</Link>
+          </header>
+          
+          <main>
+            <Switch>
+              <Route exact path="/" component={Posts} />
+              <Route path="/post/new" component={NewPost} />
+              <Route path="/post/:id" component={Post} />
+            </Switch>
+          </main>
+        </div>
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const defaultState = {
+  isEditMode: false
+}
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  const client = new ApolloClient({
+    cache,
+    uri: 'https://api-useast.graphcms.com/v1/cjkg8z3wf0jan01fhumzt7z56/master',
+    clientState: {
+      defaults: defaultState,
+      resolvers: {}
+    }
+  })  
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>, document.getElementById('root'));
+  registerServiceWorker();
+})
+
+// Using the client and a query outside of react
+// client.query({
+  //   query: POSTS_QUERY
+  // }).then(
+    //   res => console.log(res) 
+    // )
